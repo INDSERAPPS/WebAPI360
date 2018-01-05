8c7f6d243a7a3d84b215f51eab303c2988696649
tcs-ms-poc <sreeraj.ar@outlook.com>
2018-01-05 05:31:42 -0500

    Add project files.

---
+++ webapi360/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace webapi360.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ webapi360/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(webapi360.Startup))]
+
+namespace webapi360
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
